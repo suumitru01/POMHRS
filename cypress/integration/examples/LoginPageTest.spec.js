1f361242299a2978a3df74787497698c0919247e
suumitru01 <65500546+suumitru01@users.noreply.github.com>
2021-06-09 20:15:15 +0530

    Update LoginPageTest.spec.js

--- cypress/integration/examples/LoginPageTest.spec.js
+++ cypress/integration/examples/LoginPageTest.spec.js
@@ -1,11 +1,17 @@
 /// <reference types="cypress" />
+
 import LoginPage from "../PageObjects/LoginPage.js"
 
 const lp = new LoginPage()
 describe("Test Suite", function () {
 
     it("User should be able to launch an url", function () {
+
+        lp.getAPI1()
         lp.visit()
+        cy.wait('@retrieve1')
+            .its('response.statusCode')
+            .should('equal', 200)
         cy.title().should('include', 'HRS | ClinicianConnect')
         cy.url().should('include', 'login')
         cy.document().should('have.property', 'charset').and('eq', 'UTF-8')
@@ -20,14 +26,30 @@ describe("Test Suite", function () {
     it("Verify that Sign In button should be clickable If UN and PW is entered", function () {
         lp.fillEmail().clear().type('sumittelmore')
         lp.fillPassword().clear().type('admin')
+        cy.contains('Sign In').should('be.visible')
+        lp.getAPI2()
         lp.submitClickable().click()
-        cy.wait(3000)
+        cy.wait('@retrieve2')
+            .its('response.statusCode')
+            .should('equal', 422)
+        lp.notifyClose().click()
+    })
+
+    it("Verify that system should display warning message if entered UN and PW is wrong", function () {
+        lp.fillEmail().clear().type('abcs')
+        lp.fillPassword().clear().type('abcd')
+        lp.getAPI2()
+        lp.submitClickable().click()
+        cy.wait('@retrieve2')
+            .its('response.statusCode')
+            .should('equal', 422)
         lp.notify().each((note) => {
-           const state= cy.log(note.text());
+            const state = cy.log(note.text());
             state.should('contain', 'Username and/or password invalid')
-            .should('have.css', 'background-color', 'rgb(251, 67, 74)')
+                .should('have.css', 'background-color', 'rgb(251, 67, 74)')
         })
         lp.notifyClose().click()
+        cy.wait(2000)
     })
 
     it("Warning message should be disaplyed if only UN is entered and clicked on SignIn", function () {
@@ -50,28 +72,34 @@ describe("Test Suite", function () {
 
     it("Click on Reset Submit button without entering UN", function () {
         lp.forgotPassword().click()
+        lp.getAPI3()
         lp.resetPwdSubmit().click()
+        cy.wait('@retrieve3')
+            .its('response.statusCode')
+            .should('equal', 422)
         lp.notify().each((note) => {
-            const state= cy.log(note.text());
-             state.should('contain', 'Failed to submit reset password request.')
-             .should('have.css', 'background-color', 'rgb(251, 67, 74)')
-         })
+            const state = cy.log(note.text());
+            state.should('contain', 'Failed to submit reset password request.')
+                .should('have.css', 'background-color', 'rgb(251, 67, 74)')
+        })
         cy.wait(3000)
         lp.notifyClose().click()
     })
 
     it("Enter UN to reset and click on submit", function () {
         lp.resetPwdUsername().clear().type('sumit')
+        lp.getAPI4()
         lp.resetPwdSubmit().click()
+        cy.wait('@retrieve4')
+            .its('response.statusCode')
+            .should('equal', 204)
         lp.notify().each((note) => {
-            const state= cy.log(note.text());
-             state.should('contain', 'Reset password request successfully submitted.')
-             .should('have.css', 'background-color', 'rgb(70, 190, 138)')
-         })
+            const state = cy.log(note.text());
+            state.should('contain', 'Reset password request successfully submitted.')
+            state.should('have.css', 'background-color', 'rgb(70, 190, 138)')
+        })
         cy.wait(3000)
         lp.notifyClose().click()
         cy.url().should('include', 'login')
     })
 })
-
-
